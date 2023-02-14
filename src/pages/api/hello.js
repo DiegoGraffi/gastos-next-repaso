c9df325c5e21cd90395021a0eb22a52daa2f76e8
Diego Graffigna <diegograffigna1@gmail.com>
2023-02-13 23:18:55 -0300

    repaso01

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { supabase } from "@/lib/supabaseClient";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET": {
+      const { data, error } = await supabase.from("gastos-repaso").select();
+      console.log(data);
+      res.status(200).json(data);
+      break;
+    }
+    case "POST": {
+      const gasto = req.body;
+      const { data, error } = await supabase
+        .from("gastos-repaso")
+        .insert({
+          nombre: gasto.nombre,
+          cantidad: gasto.cantidad,
+          categoria: gasto.categoria,
+        })
+        .select();
+
+      if (error !== null) {
+        console.log(error);
+        res.status(400).send(error.message);
+      } else {
+        res.status(200).json(data[0]);
+      }
+      break;
+    }
+    case "DELETE": {
+      const id = req.query.id;
+      const { error } = await supabase
+        .from("gastos-repaso")
+        .delete()
+        .eq("id", id);
+      res.status(200).json({ message: "Borrado exitosamente" });
+      break;
+    }
+
+    default:
+      res.status(200).json({ message: "metodo no soportado" });
+  }
 }
